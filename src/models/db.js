8098a8f1e7fde3d04535453590aa1947129634bd
Nghia Nguyen <nghianguyen3004@gmail.com>
2026-05-26 12:38:38 +0700

    Recover the debug code

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,14 @@
 import {Pool} from 'pg';
 const pool = new Pool({connectionString: process.env.DATABASE_URL});
 
-
+/*
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error executing query', err.stack);
+    } else {
+        console.log('Current time:', res.rows[0].now);
+    }
+});
+*/
 
 export default pool;
