5b1c5c580d22a2410d7d693513b8828aa63e9049
Nghia Nguyen <nghianguyen3004@gmail.com>
2026-05-27 09:16:32 +0700

    Finished the login controller.

---
+++ src/controllers/login.js
@@ -0,0 +1,31 @@
+import {getUserByEmail} from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        if(!email || !password){
+            return res.status(400).json({error: 'Fields cannot be empty'});
+        } if (!email.includes('@') || !email.includes('.')){
+            return res.status(400).json({error: 'Invalid email format'});
+        } else{
+            const getUser = await getUserByEmail(email);
+            if (!getUser){
+                return res.status(404).json({error: 'User not found'});
+            } else{
+                const passwordMatch = await bcrypt.compare(password, getUser.password);
+                if (!passwordMatch){
+                    return res.status(401).json({error: 'Incorrect password'});
+                } else{
+                    const token = jwt.sign({ userID: getUser.userid }, JWT_SECRET, { expiresIn: '1h' });
+                    res.status(200).json({ message: 'Login successful', token });
+                }
+            }
+        }
+    } catch (error){
+        throw error;
+    }
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const registerController = await import('../controllers/register.js');
+const loginController = await import('../controllers/login.js');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
 
     registerController.register(req, res);
+});
+
+router.post('/login', async (req, res) => {
+    loginController.login(req, res);
 });
 
 
