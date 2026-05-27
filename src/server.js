87db5bc154b986b1eca68f5eeb4a7f7a4310e264
Nghia Nguyen <nghianguyen3004@gmail.com>
2026-05-27 13:31:11 +0700

    Tested Cors

--- src/server.js
+++ src/server.js
@@ -6,52 +6,24 @@ import { createUser, getUserByEmail } from './models/userModel.js';
 import router from './routes/authRoutes.js';
 import todoRouter from './routes/todoRoutes.js';
 import { requireAuth } from './middleware/authMiddle.js';
+import cors from 'cors';
 
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies to be sent with requests
+}));
 app.use(express.json());
 app.use('/auth', router);
 app.use('/todos', todoRouter);
 
+
 app.get('/', async (req, res) => {
-    //res.json({ status: 'OK' });
-    const allTodos = await getAllTodos(1);
-    //console.log(allTodos);
-
-    //Testing code
-    /*
-    console.log("After creating a new todo");
-    const createTodo = await createATodo('test todo 2', 1);
-    console.log(allTodos);
-    */
-
-    //Testing updating function
-    /*
-    const titleChange = await changingTheTitle("To do changed again", 3);
-    console.log(titleChange);
-
-    const taskCompleted = await completedATask(3);
-    console.log(taskCompleted);
-    */
-
-    //Testing deleting function
-    //const deleteTask = await deleteATask(5);
-    //console.log(allTodos);
-
-    //Testing create a user function
-    //const newlyCreatedUser = await createUser('Nghia2', 'abcd@gmail.com', '147891');
-    //console.log(newlyCreatedUser);
-
-    //Testing get user by email function
-    /*
-    const getUserEmail = await getUserByEmail('abc@gmail.com');
-    console.log(getUserEmail);
-    const getUser2Email = await getUserByEmail('abcd@gmail.com');
-    console.log(getUser2Email);
-    */
-
-
+    res.json({ status: 'OK' });
 });
 
 
